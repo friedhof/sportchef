3f3252bd3c8f26dcae8a596cb6a86c754d590981
Marcus Fihlon <marcus@fihlon.ch>
2015-02-03 17:18:53 +0100

    create adapter to (de)serialize LocalDate instances

---
+++ src/main/java/ch/sportchef/business/adapter/LocalDateAdapter.java
@@ -0,0 +1,23 @@
+package ch.sportchef.business.adapter;
+
+import javax.xml.bind.annotation.adapters.XmlAdapter;
+import java.time.LocalDate;
+
+public class LocalDateAdapter extends XmlAdapter<String, LocalDate> {
+
+    @Override
+    public String marshal(final LocalDate localDate) throws Exception {
+        return String.format("%s-%s-%s",
+                localDate.getYear(), localDate.getMonth(), localDate.getDayOfMonth());
+    }
+
+    @Override
+    public LocalDate unmarshal(final String string) throws Exception {
+        final String[] data = string.split("\\-");
+        final int year = Integer.parseInt(data[0]);
+        final int month = Integer.parseInt(data[1]);
+        final int dayOfMonth = Integer.parseInt(data[2]);
+        return LocalDate.of(year, month, dayOfMonth);
+    }
+
+}
