8a1be620cdac222096d03dbbabe4da378101b123
Marcus Fihlon <marcus@fihlon.ch>
2015-02-04 17:19:07 +0100

    create adapter to (de)serialize LocalTime instances

---
+++ src/main/java/ch/sportchef/business/adapter/LocalTimeAdapter.java
@@ -0,0 +1,22 @@
+package ch.sportchef.business.adapter;
+
+import javax.xml.bind.annotation.adapters.XmlAdapter;
+import java.time.LocalTime;
+
+public class LocalTimeAdapter extends XmlAdapter<String, LocalTime> {
+
+    @Override
+    public String marshal(final LocalTime localTime) throws Exception {
+        return String.format("%s:%s",
+                localTime.getHour(), localTime.getMinute());
+    }
+
+    @Override
+    public LocalTime unmarshal(final String string) throws Exception {
+        final String[] data = string.split("\\:");
+        final int hour = Integer.parseInt(data[0]);
+        final int minute = Integer.parseInt(data[1]);
+        return LocalTime.of(hour, minute);
+    }
+
+}
