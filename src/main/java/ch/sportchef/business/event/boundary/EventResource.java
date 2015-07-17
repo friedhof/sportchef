adf3d1c29288c328f724c37248af8a301d3fa33d
Marcus Fihlon <marcus@fihlon.ch>
2015-07-17 13:30:40 +0200

    #42 Add missing import

--- src/main/java/ch/sportchef/business/event/boundary/EventResource.java
+++ src/main/java/ch/sportchef/business/event/boundary/EventResource.java
@@ -26,6 +26,7 @@ import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.NotFoundException;
 import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
