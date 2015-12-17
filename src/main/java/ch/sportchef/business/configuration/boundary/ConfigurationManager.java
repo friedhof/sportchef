a0c1e76327516d2988de343fec1422d7e2698458
Marcus Fihlon <marcus@fihlon.ch>
2015-12-17 15:13:54 +0100

    #119 Type of field 'configuration' is concrete class 'Configuration'

--- src/main/java/ch/sportchef/business/configuration/boundary/ConfigurationManager.java
+++ src/main/java/ch/sportchef/business/configuration/boundary/ConfigurationManager.java
@@ -34,6 +34,7 @@ public class ConfigurationManager {
     private  static final String DEFAULT_CONFIGURATION_FILE = "cfg_default.properties"; //NON-NLS
     private  static final String CUSTOM_CONFIGURATION_FILE = "cfg_custom.properties"; //NON-NLS
 
+    @SuppressWarnings("InstanceVariableOfConcreteClass")
     private final Configuration configuration;
 
     public ConfigurationManager() {
