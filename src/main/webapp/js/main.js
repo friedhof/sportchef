fdc03d21b3db7c8f83defd23454304564cd0b1f0
Dominik Müller <d.mueller@iwf.ch>
2015-12-31 11:06:16 +0100

    #30: Completed translations

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -18,50 +18,118 @@
 
 "use strict";
 
+/**
+ * Locales which are supported for translations
+ *
+ * @type {{en: string}}
+ */
 var locales = {
     'en': 'english'
 };
 
+
 var translations = {
-    init: function(){
-        for (var locale in locales){
+    init: function () {
+        var translationContainer = document.createElement("div");
+        translationContainer.setAttribute("id", "translation-container");
+        var mainElement = document.getElementsByTagName("main")[0];
+        var translationLink = this.createTranslationLink("de", "deutsch");
+        translationContainer.appendChild(translationLink);
+
+        for (var locale in locales) {
             this[locale] = [];
             var that = this;
-            this.loadTranslation(locale, function(data){
+            this.loadTranslation(locale, function (data) {
                 that[locale] = JSON.parse(data);
             });
-            this.translate();
+            translationLink = this.createTranslationLink(locale, locales[locale]);
+            translationContainer.appendChild(translationLink);
+        }
+
+        mainElement.insertBefore(translationContainer, mainElement.childNodes[0]);
+
+        this.addEventListeners();
+
+        var currentLanguage = sessionStorage.getItem("language");
+        if (currentLanguage !== null && typeof currentLanguage !== 'undefined'){
+            this.translate(currentLanguage);
         }
     },
-    translate: function(){
-        var untranslatedElements = document.querySelectorAll('[data-translate]');
+    createTranslationLink: function (locale, desc) {
+        var translationLink = document.createElement("a");
 
-        for (var untranslatedElement in untranslatedElements){
+        translationLink.appendChild(document.createTextNode(locale));
+        translationLink.setAttribute("href", "#");
+        translationLink.setAttribute("class", "translation-link");
+        translationLink.setAttribute("title", desc);
+
+        return translationLink;
+    },
+    addEventListeners: function () {
+        var translator = this;
+        var translationLinks = document.getElementsByClassName('translation-link');
+        for (var translationLink in translationLinks) {
+            if (!isNaN(parseFloat(translationLink)) && isFinite(translationLink)){
+                translationLinks[translationLink].addEventListener('click', function () {
+                    var locale = this.textContent;
+                    if (locale !== "de"){
+                        translator.translate(locale);
+                        sessionStorage.setItem("language", locale);
+                    } else{
+                        translator.reverseTranslate();
+                    }
+                });
+            }
+        }
+    },
+    translate: function (locale) {
+        if (typeof locale === 'undefined' || !this.exists(locale)){
+            return;
+        }
+        var untranslatedElements = document.querySelectorAll('[data-translatable]');
+
+        for (var untranslatedElement in untranslatedElements) {
             if (!isNaN(parseFloat(untranslatedElement)) && isFinite(untranslatedElement)){ // If it is actual an element and not length or item property.
                 var el = untranslatedElements[untranslatedElement];
-                var desiredLocale = el.dataset.translate;
-                if (translations.exists(desiredLocale)){
-                    var possibleTranslation =  translations[desiredLocale][el.textContent];
+                if (translations.exists(locale)){
+                    var possibleTranslation = translations[locale][el.textContent];
                     if (typeof possibleTranslation === 'string'){
                         el.textContent = possibleTranslation;
+                        el.setAttribute('lang', locale);
                     }
                 }
             }
         }
     },
-    loadTranslation: function(locale, callback){
+    reverseTranslate: function(){
+        var locale = sessionStorage.getItem("language");
+        var translatedElements = document.querySelectorAll('[data-translatable]');
+
+        for (var translatedElement in translatedElements){
+            if (!isNaN(parseFloat(translatedElement)) && isFinite(translatedElement)){
+                var el = translatedElements[translatedElement];
+                for (var word in this[locale]){
+                    if (el.textContent === this[locale][word]){
+                        el.textContent = word;
+                        break;
+                    }
+                }
+            }
+        }
+        sessionStorage.removeItem("language");
+    },
+    loadTranslation: function (locale, callback) {
         var xobj = new XMLHttpRequest();
         xobj.overrideMimeType("application/json");
-        xobj.open('GET', 'js/translations.'+locale+'.json', false); // Replace 'my_data' with the path to your file
+        xobj.open('GET', 'js/translations.' + locale + '.json', false);
         xobj.onreadystatechange = function () {
-            if (xobj.readyState == 4 && xobj.status == "200") {
-                // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
+            if (xobj.readyState == 4 && xobj.status == "200"){
                 callback(xobj.responseText);
             }
         };
         xobj.send(null);
     },
-    exists: function(locale){
+    exists: function (locale) {
         return this[locale] !== null && typeof this[locale] !== 'undefined';
     }
 };
