38537f423dede1bf1ae9ff1ebf5a1fc54fff8464
Marcus Fihlon <marcus@fihlon.ch>
2016-02-20 20:42:56 +0100

    #164 Wrote a test for the Configuration class

--- src/test/java/ch/sportchef/business/configuration/entity/ConfigurationTest.java
+++ src/test/java/ch/sportchef/business/configuration/entity/ConfigurationTest.java
@@ -19,10 +19,16 @@ package ch.sportchef.business.configuration.entity;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.junit.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.startsWith;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static java.lang.Boolean.FALSE;
 
 public class ConfigurationTest {
 
@@ -42,4 +48,43 @@ public class ConfigurationTest {
         configuration = null;
     }
 
+    @Test
+    public void getAdminPassword() {
+        assertThat(configuration.getAdminPassword(), is("test.admin.password"));
+    }
+
+    @Test
+    public void getSMTPServer() {
+        assertThat(configuration.getSMTPServer(), is("test.smtp.server"));
+    }
+
+    @Test
+    public void getSMTPPort() {
+        assertThat(configuration.getSMTPPort(), is(25));
+    }
+
+    @Test
+    public void getSMTPUser() {
+        assertThat(configuration.getSMTPUser(), is("test.smtp.user"));
+    }
+
+    @Test
+    public void getSMTPPassword() {
+        assertThat(configuration.getSMTPPassword(), is("test.smtp.password"));
+    }
+
+    @Test
+    public void getSMTPSSL() {
+        assertThat(configuration.getSMTPSSL(), is(FALSE));
+    }
+
+    @Test
+    public void getSMTPFrom() {
+        assertThat(configuration.getSMTPFrom(), is("test@smtp.from"));
+    }
+
+    @Test
+    public void toStringTest() {
+        assertThat(configuration.toString(), startsWith("Configuration{properties="));
+    }
 }
