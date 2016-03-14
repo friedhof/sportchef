140e7b328bad2428f345406a004096e8b92d39cf
Marcus Fihlon <marcus@fihlon.ch>
2016-03-14 21:25:34 +0100

    #198 Wrote tests for ConfigurationService class

---
+++ src/test/java/ch/sportchef/business/configuration/control/ConfigurationServiceTest.java
@@ -0,0 +1,68 @@
+/*
+ * SportChef – Sports Competition Management Software
+ * Copyright (C) 2016 Marcus Fihlon
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package ch.sportchef.business.configuration.control;
+
+import ch.sportchef.business.configuration.entity.Configuration;
+import org.junit.Rule;
+import org.junit.Test;
+import org.needle4j.annotation.ObjectUnderTest;
+import org.needle4j.junit.NeedleBuilders;
+import org.needle4j.junit.NeedleRule;
+
+import javax.inject.Inject;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class ConfigurationServiceTest {
+
+    @Rule
+    public NeedleRule needleRule = NeedleBuilders.needleMockitoRule().build();
+
+    @ObjectUnderTest
+    private ConfigurationService configurationService;
+
+    @Inject
+    private ConfigurationRepository configurationRepositoryMock;
+
+    @Test
+    public void getConfiguration() {
+        // arrange
+        final Configuration configurationMock = mock(Configuration.class);
+        when(configurationRepositoryMock.getConfiguration()).thenReturn(configurationMock);
+
+        // act
+        final Configuration configuration = configurationService.getConfiguration();
+
+        // assert
+        assertThat(configuration, is(configurationMock));
+    }
+
+    @Test
+    public void toStringTest() {
+        // arrange
+
+        // act
+        final String toString = configurationService.toString();
+
+        // assert
+        assertThat(toString, is("ConfigurationService(configurationRepository=Mock for ConfigurationRepository, hashCode: 1888442711)"));
+    }
+}
