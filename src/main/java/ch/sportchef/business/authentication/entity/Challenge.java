4cec5e193cb6f7bbe1438f3c952551df52433a8f
Marcus Fihlon <marcus@fihlon.ch>
2016-04-30 19:40:10 +0200

    #243 Deleted the constructor because the constructor can be automatically generated by lombok

--- src/main/java/ch/sportchef/business/authentication/entity/Challenge.java
+++ src/main/java/ch/sportchef/business/authentication/entity/Challenge.java
@@ -1,18 +1,16 @@
 package ch.sportchef.business.authentication.entity;
 
 import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import lombok.ToString;
 
+@RequiredArgsConstructor
 @Getter
 @ToString
 public class Challenge {
 
     private final String challenge;
     private int tries = 0;
-
-    public Challenge(String challenge) {
-        this.challenge = challenge;
-    }
 
     public void increaseTries() {
         tries++;
