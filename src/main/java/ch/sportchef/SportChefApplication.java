459be4879f76e1f6f4eb53057a555e203d4ae434
Marcus Fihlon <marcus@fihlon.ch>
2016-05-05 16:19:31 +0200

    #270 Basic configuration of Dropwizard framework

---
+++ src/main/java/ch/sportchef/SportChefApplication.java
@@ -0,0 +1,58 @@
+/*
+ * SportChef – Sports Competition Management Software
+ * Copyright (C) 2016 Marcus Fihlon
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package ch.sportchef;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+import io.dropwizard.Application;
+import io.dropwizard.jersey.setup.JerseyEnvironment;
+import io.dropwizard.setup.Environment;
+import lombok.SneakyThrows;
+
+import javax.validation.constraints.NotNull;
+
+public class SportChefApplication extends Application<SportChefConfiguration> {
+
+    @SneakyThrows
+    public static void main(@NotNull final String... args) {
+        new SportChefApplication().run(args);
+    }
+
+    @Override
+    public void run(@NotNull final SportChefConfiguration configuration,
+                    @NotNull final Environment environment) {
+        final Injector injector = createInjector(configuration);
+        registerResources(environment, injector);
+    }
+
+    private Injector createInjector(@NotNull final SportChefConfiguration configuration) {
+        return Guice.createInjector(new AbstractModule() {
+            @Override
+            protected void configure() {
+                bind(SportChefConfiguration.class).toInstance(configuration);
+            }
+        });
+    }
+
+    private void registerResources(@NotNull final Environment environment,
+                                   @NotNull final Injector injector) {
+        final JerseyEnvironment jersey = environment.jersey();
+    }
+
+}
