ab0ba9f4df8a0dd31a89e9d4839405b4fbdbb043
Marcus Fihlon <marcus@fihlon.ch>
2016-09-01 21:59:55 +0200

    Corrected a problem with determining the data directory

--- src/main/java/ch/sportchef/business/PersistenceManager.java
+++ src/main/java/ch/sportchef/business/PersistenceManager.java
@@ -22,31 +22,23 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import pl.setblack.airomem.core.SimpleController;
 
+import java.io.File;
 import java.io.Serializable;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.function.Supplier;
 
 @UtilityClass
 public class PersistenceManager {
 
-    private static final String SPORTCHEF_DIRECTORY_NAME = ".sportchef"; //NON-NLS
-    private static final String PREVAYLER_DIRECTORY_NAME = "prevayler"; //NON-NLS
-
-    private static final Path DATA_DIRECTORY;
-
     private static final Logger LOGGER = LoggerFactory.getLogger(PersistenceManager.class);
-
-    static {
-        DATA_DIRECTORY = Paths.get(SPORTCHEF_DIRECTORY_NAME, PREVAYLER_DIRECTORY_NAME);
-    }
 
     public static <T extends Serializable> SimpleController<T> createSimpleController(
             final Class<? extends Serializable> clazz, final Supplier<T> constructor) {
-        final String dir = DATA_DIRECTORY.resolve(clazz.getName()).toString();
-        LOGGER.info("Using persistence store '{}' for entity '{}'.",
-                Paths.get(System.getProperty("user.home"), dir).toAbsolutePath(),
+        final String dir = String.format("%s%s.sportchef%sprevayler%s%s", //NON-NLS
+                System.getProperty("user.home"), //NON-NLS
+                File.separator, File.separator, File.separator,
                 clazz.getName());
+        LOGGER.info("Using persistence store '{}' for entity '{}'.", //NON-NLS
+                dir, clazz.getName());
         return SimpleController.loadOptional(dir, constructor);
     }
 
