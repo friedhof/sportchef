7fd61d82a7c4d250070f034f3bdd4225914d1556
Marcus Fihlon <marcus@fihlon.ch>
2016-09-08 20:06:03 +0200

    #287 New configuration file

---
+++ src/main/java/ch/sportchef/JAXRSConfiguration.java
@@ -0,0 +1,9 @@
+package ch.sportchef;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("api")
+public class JAXRSConfiguration extends Application {
+
+}
