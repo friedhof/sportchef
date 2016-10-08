f0bdd61f2702ad7c0e694114bf8cb1380b2c8127
Marcus Fihlon <marcus@fihlon.ch>
2016-10-08 13:24:43 +0200

    PingResource returns plain text, not JSON

--- src/main/java/ch/sportchef/business/monitoring/boundary/PingResource.java
+++ src/main/java/ch/sportchef/business/monitoring/boundary/PingResource.java
@@ -24,7 +24,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 @Path("pings")
-@Produces(MediaType.APPLICATION_JSON)
+@Produces(MediaType.TEXT_PLAIN)
 public class PingResource {
 	
 	@GET
